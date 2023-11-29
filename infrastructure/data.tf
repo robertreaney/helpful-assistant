0a8e11380cdaa4becade3fde95c8df0ba98f71d9
Robert <robertlreaney@gmail.com>
2023-11-28 21:55:44 -0500

    refactor

--- infrastructure/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-data "template_file" "ec2-startup" {
-  template = file("${path.module}/ec2_startup.sh")
-
-  # vars = {
-  #   DOCKER_IMAGE_NAME = docker_registry_image.website.name
-  # }
-}
--- infrastructure/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "helpful_ai_server_eip" {
-  value       = aws_eip.helpful_ai_server.public_ip
-  description = "Elastic IP of Helpful AI server."
-}
